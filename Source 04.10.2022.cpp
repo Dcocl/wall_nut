9a3447323ab3db03fefe610f4c8004722c8db948
Dcocl <114007502+Dcocl@users.noreply.github.com>
2022-10-04 18:36:56 +0300

    Add files via upload

---
+++ Source 04.10.2022.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUS");
+		/*Case1. Дано целое число в диапазоне 1–7. Вывести строку — название дня
+	недели, соответствующее данному числу (1 — «понедельник», 2 — «вторник*/
+	/*
+	int  day;
+	cin >> day;
+	if (day == 1); {
+		cout << "понедельник" << endl
+	}
+	else if (day == 2); {
+		cout << "вторник" << endl
+	}
+	else if (day == 7); {
+		cout << "воскресенье" << endl
+		}
+		else {
+		cout << "ошибочный номер дня" << endl
+		}*/
+	/*int day;
+	cin >> day;
+	switch (day) {
+	case 1: cout << "понедельник" << endl; break;
+	case 2: cout << "вторник" << endl; break;
+	case 3: cout << "среда" << endl; break;
+	case 4: cout << "четверг" << endl; break;
+	case 5: cout << "пятница" << endl; break;
+	case 6: cout << "суббота" << endl; break;
+	case 7: cout << "воскресенье" << endl; break;
+	default: cout << "Ошибочный номер дня" << endl;
+	}*/
+
+	
+	//Case2. Дано целое число K. Вывести строку-описание оценки, соответствующей числу K (1 — «плохо», 2 — «неудовлетворительно», 3 — «удовлетворительно», 4 — «хорошо», 5 — «отлично»). Если K не лежит в диапазоне 1–5,то вывести строку «ошибка».
+	/*int k;
+	cin >> k;
+	switch (k) {
+	case 1: cout << "плохо" << endl; break;
+	case 2: cout << "неудовлетворительно" << endl; break;
+	case 3: cout << "удовлетворительно" << endl; break;
+	case 4: cout << "хорошо" << endl; break;
+	case 5: cout << "отлично" << endl; break;
+	default : cout << "ошибка" << endl; break;
+	}*/
+	//Case4◦. Дан номер месяца — целое число в диапазоне 1–12(1 — январь, 2 —февраль и т.д.).Определить количество дней в этом месяце для невисокосного года.
+	/*int month;
+	cin >> month;
+	switch (month) {
+	case 1:cout << "31" << endl; break;
+	case 2:cout << "28" << endl; break;
+	case 3:cout << "31" << endl; break;
+	case 4:cout << "31" << endl; break;
+	case 5:cout << "31" << endl; break;
+	case 6:cout << "31" << endl; break;
+	case 7:cout << "31" << endl; break;
+	case 8:cout << "31" << endl; break;
+	case 9:cout << "30" << endl; break;
+	case 10:cout << "31" << endl; break;
+	case 11:cout << "30" << endl; break;
+	case 12:cout << "31" << endl; break;
+	}*/
+	/*//1
+	int a;
+	cin >> a;
+	if (a == 0) {
+		cout << "верно" << endl;
+	}
+	else (a == 0); {
+		cout << "неверно" << endl;
+	}*/
+	/*//2
+	int a;
+	cin >> a;
+	if (a > 0) {
+		cout << "верно" << endl;
+	}
+	else (a > 0); {
+		cout << "неверно" << endl;
+	}*/
+	/*//3
+	int a;
+	cin >> a;
+	if (a < 0) {
+		cout << "верно" << endl;
+	}
+	else (a < 0); {
+		cout << "неверно" << endl;
+	}*/
+	/*//4
+	int a;
+	cin >> a;
+	if (a >= 0) {
+		cout << "верно" << endl;
+	}
+	else (a >= 0); {
+		cout << "неверно" << endl;
+	}*/
+	/*//5
+	int a;
+	cin >> a;
+	if (a <= 0) {
+		cout << "верно" << endl;
+	}
+	else (a <= 0); {
+		cout << "неверно" << endl;
+	}*/
+	/*//6
+	int a;
+	cin >> a;
+	if (a != 0) {
+		cout << "верно" << endl;
+	}
+	else (a != 0); {
+		cout << "неверно" << endl;
+	}*/
+/*//9
+bool test = true;
+if (test = true) {
+	cout << "верно" << endl;
+}
+else (test =true); {
+	cout << "неверно" << endl;
+}*/
+     
+
+
+    return 0;
+
+
+}
